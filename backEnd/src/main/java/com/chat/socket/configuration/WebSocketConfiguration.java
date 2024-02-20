198b694d64bfe75935908e99c8573e9121339d63
Nathan Ollichon <nathan.ollichon@ausy.fr>
2024-02-20 13:52:30 +0100

    fix double package declaration

--- backEnd/src/main/java/com/chat/socket/configuration/WebSocketConfiguration.java
+++ backEnd/src/main/java/com/chat/socket/configuration/WebSocketConfiguration.java
@@ -1,7 +1,5 @@
 package com.chat.socket.configuration;
 
-package com.chat.socket.configuration;
-
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
