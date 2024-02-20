88068f10f309ed6c1ea0a21076793e8f6743cf84
NathanOllichon <ollichon.nathan@gmail.com>
2024-02-20 13:49:13 +0100

    config socketURL for connection

--- backEnd/src/main/java/com/chat/kafka/KafkaTopicConfig.java
+++ backEnd/src/main/java/com/chat/kafka/KafkaTopicConfig.java
@@ -17,14 +17,14 @@ public class KafkaTopicConfig {
     private String bootstrapAddress;
 
     @Bean
-    public KafkaAdmin kafkaAdmin() {
+    KafkaAdmin kafkaAdmin() {
         Map<String, Object> configs = new HashMap<>();
         configs.put(AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapAddress);
         return new KafkaAdmin(configs);
     }
     
     @Bean
-    public NewTopic topic1() {
+    NewTopic topic1() {
          return new NewTopic("TryTopic", 1, (short) 1);
     }
 }
