3f5d19aebf7fa6db3ebf295a2bb052a469b1b969
NathanOllichon <ollichon.nathan@gmail.com>
2024-02-28 16:12:20 +0100

    add general chatComponent for ClientService with good userId

--- frontEnd/chat-client-service/src/app/component/client-service-chat/client-service-chat.component.ts
+++ frontEnd/chat-client-service/src/app/component/client-service-chat/client-service-chat.component.ts
@@ -1,10 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ChatMessage } from 'src/app/models/ChatMessage';
+import { ChatService } from 'src/app/services/ChatService';
 
 @Component({
   selector: 'app-client-service-chat',
   templateUrl: './client-service-chat.component.html',
   styleUrls: ['./client-service-chat.component.css']
 })
-export class ClientServiceChatComponent {
+export class ClientServiceChatComponent implements OnInit {
 
+  messageInput: string = '';
+  userId:     string="00000001";
+  receiverId: string="00000046";
+  messageList: any[] = [];
+
+  constructor(private chatService: ChatService){}
+
+  ngOnInit(): void {
+    this.chatService.joinRoom("00000046");
+    this.lisenerMessage();
+  }
+
+  sendMessage() {
+    const chatMessage = {
+      message: this.messageInput,
+      producer: this.userId,
+      receiver: this.receiverId
+    }as ChatMessage
+    this.chatService.sendMessage(this.userId, chatMessage);
+    this.messageInput = '';
+  }
+
+  lisenerMessage() {
+    this.chatService.getMessageSubject().subscribe((messagesJSON: any[]) => {
+
+      this.messageList = messagesJSON.map((item: ChatMessage)=> (
+        {
+        ...item,
+        message_side: item.producer === this.userId ? 'sender': 'receiver'
+        })
+      )
+    });
+  }
 }
